154b08bf63c16c9331984754f8be823c74434709
Ambar <ambarsquintana@gmail.com>
2020-06-07 14:13:49 -0400

    add test

---
+++ tests/Feature/UserModuleTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserModuleTest extends TestCase
+{
+    /** @test */
+    function it_loads_the_users_list_page()
+    {
+        $this->get('/usuarios')
+            ->assertStatus(200)
+            ->assertSee('Usuarios');
+    }
+
+    /** @test */
+    function it_loads_the_users_details_page()
+    {
+        $this->get('/usuarios/5')
+            ->assertStatus(200)
+            ->assertSee('Mostrando detalles del usuario 5');
+    }
+
+    /** @test */
+    function it_loads_the_new_users_page()
+    {
+        $this->get('/usuarios/nuevo')
+            ->assertStatus(200)
+            ->assertSee('Crear nuevo usuario');
+    }
+
+    /** @test */
+    function it_loads_the_edit_users_page()
+    {
+        $this->get('/usuarios/5/edit')
+            ->assertStatus(200)
+            ->assertSee('Editando usuario 5');
+    }
+}
